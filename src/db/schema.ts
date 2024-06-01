8944473f42e27902d5dc1386687b65783bdc4a63
Deepak <deepaktomson@gmail.com>
2024-06-01 18:42:56 +0530

    Initial setup with postgres database, docker and drizzle orm

---
+++ src/db/schema.ts
@@ -0,0 +1,11 @@
+import { serial } from "drizzle-orm/pg-core";
+import {
+ 
+  pgTable,
+
+} from "drizzle-orm/pg-core";
+
+export const bids = pgTable("bids", {
+    id: serial("id").primaryKey()
+})
+
